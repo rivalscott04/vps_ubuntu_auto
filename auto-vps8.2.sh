18c4252085b642ffe8c3a9aa4e4b4aca31925a22
rivalscott04 <rival.biasrori@outlook.com>
2025-07-15 17:40:08 +0800

    refactor(auto-vps): update main menu options and streamline Node.js configuration
    This commit modifies the main menu in the `auto-vps8.2.sh` script to improve clarity by adjusting the layout of installation options. The configuration for Node.js with systemd is now streamlined, and unnecessary entries have been removed to enhance user experience and organization of the menu.

--- auto-vps8.2.sh
+++ auto-vps8.2.sh
@@ -47,9 +47,9 @@ while true; do
     echo "├───────────────────────────────────────────────┼───────────────────────────────────────────────┼───────────────────────────────────────────────┤"
     printf "│ %-37s │ %-37s │ %-37s │\n" "1. Install PHP" "10. Optimasi Server" "14. Tampilkan Info Sistem"
     printf "│ %-37s │ %-37s │ %-37s │\n" "2. Install Nginx" "11. Instalasi Sistem Cache" "15. Aktifkan SSL Semua Domain"
-    printf "│ %-37s │ %-37s │ %-37s │\n" "3. Install Database" "12. Security Hardening" "16. Cek Ekstensi PHP"
-    printf "│ %-37s │ %-37s │ %-37s │\n" "4. Install phpMyAdmin" "13. Sistem Backup" "17. Cek Ekstensi PHP"
-    printf "│ %-37s │ %-37s │ %-37s │\n" "5. Install Node.js & npm" "" "18. Konfigurasi systemd Node.js"
+    printf "│ %-37s │ %-37s │ %-37s │\n" "3. Install Database" "12. Security Hardening" "16. Konfigurasi systemd Node.js"
+    printf "│ %-37s │ %-37s │ %-37s │\n" "4. Install phpMyAdmin" "13. Sistem Backup" ""
+    printf "│ %-37s │ %-37s │ %-37s │\n" "5. Install Node.js & npm" "" ""
     printf "│ %-37s │ %-37s │ %-37s │\n" "6. Install FrankenPHP" "" ""
     printf "│ %-37s │ %-37s │ %-37s │\n" "7. Install WordPress" "" ""
     printf "│ %-37s │ %-37s │ %-37s │\n" "8. Konfigurasi Aplikasi Web" "" ""
@@ -75,8 +75,7 @@ while true; do
         13) setup_backup_system ;;
         14) bash ./systeminfo.sh ;;
         15) offer_ssl_for_all_domains ;;
-        16) check_installed_php_extensions ;;
-        17) configure_nodejs_systemd ;;
+        16) configure_nodejs_systemd ;;
         0) log_info "Terima kasih telah menggunakan script ini!"
            exit 0 ;;
         *) log_error "Pilihan tidak valid" ;;
