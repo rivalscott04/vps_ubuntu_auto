5d9ffc80f7c8ad1fbabf7762c80445dc6ed5d607
rivalscott04 <114844785+rivalscott04@users.noreply.github.com>
2025-11-25 17:03:43 +0800

    revisi nginx conf khusus express

--- configurators.sh
+++ configurators.sh
@@ -240,7 +240,6 @@ configure_webapp_path_based() {
             return
             ;;
     esac
-    
     # Array untuk menyimpan konfigurasi aplikasi
     declare -a apps_config
     apps_count=0
@@ -303,7 +302,7 @@ configure_webapp_path_based() {
     safe_name=$(echo "$main_server" | sed 's/\./_/g')
     nginx_conf="/etc/nginx/sites-available/${safe_name}-pathbased"
     
-    cat > "$nginx_conf" <<EOF
+cat > "$nginx_conf" <<EOF
 server {
     listen 80;
     server_name ${main_server};
@@ -388,16 +387,18 @@ EOF
             nodejs:*)
                 node_port="${app_type#nodejs:}"
                 cat >> "$nginx_conf" <<EOF
-    location /$path_name {
+    location /$path_name/ {
         proxy_pass http://127.0.0.1:${node_port};
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
+        proxy_set_header Connection "upgrade";
         proxy_set_header Host \$host;
         proxy_cache_bypass \$http_upgrade;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header Origin \$http_origin;
+        proxy_pass_header Set-Cookie;
     }
     
 EOF
